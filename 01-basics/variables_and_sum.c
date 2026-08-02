c18e9f5cfd2b91258efc43b32e9be46733e764ca
Hamidreza <hamidrezawjabbari@gmail.com>
2026-08-02 18:40:52 +0200

    Add variables and sum calculation in C program

---
+++ 01-basics/variables_and_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int first_number = 10;
+    int second_number = 5;
+    int sum = first_number + second_number;
+
+    printf("First number: %d\n", first_number);
+    printf("Second number: %d\n", second_number);
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
